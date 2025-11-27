f48ece4b78f3d38872f85debf5343891734df113
ShivamP-97 <shivampathak19972@gmail.com>
2025-11-27 14:19:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
 //your JS code here. If required.
+const smallCups = document.querySelectorAll(".cup-small");
+const liters = document.getElementById("liters");
+const remained = document.getElementById("remained");
+const percentage = document.getElementById("percentage");
+
+smallCups.forEach((cup, idx) => {
+    cup.addEventListener("click", () => highlightCups(idx));
+});
+
+function highlightCups(idx) {
+    if (smallCups[idx].classList.contains("full") &&
+        !smallCups[idx].nextElementSibling?.classList.contains("full")) {
+        idx--;
+    }
+
+    smallCups.forEach((cup, i) => {
+        if (i <= idx) cup.classList.add("full");
+        else cup.classList.remove("full");
+    });
+
+    updateBigCup();
+}
+
+function updateBigCup() {
+    const fullCups = document.querySelectorAll(".cup-small.full").length;
+    const totalCups = smallCups.length;
+
+    if (fullCups === 0) {
+        percentage.style.height = "0";
+        percentage.innerText = "0%";
+    } else {
+        let percent = (fullCups / totalCups) * 100;
+        percentage.style.height = percent + "%";
+        percentage.innerText = `${percent}%`;
+    }
+
+    const remaining = 2 - (fullCups * 0.25);
+    liters.innerText = remaining.toFixed(2) + "L";
+
+    remained.style.height = ((2 - (fullCups * 0.25)) / 2) * 100 + "%";
+}
